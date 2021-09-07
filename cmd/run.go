448a0cb6ef3f0c5fee77b225ba9e9fa24f085aa7
MacLikorne <e.radnai@hotmail.fr>
2021-09-07 12:29:33 +0200

    failing

--- cmd/run.go
+++ cmd/run.go
@@ -6,5 +6,16 @@ func Execute(port string) {
 	routes := []pkg.Route{
 		{"test", "get"},
 	}
+
+	var toto  []string
+	print(toto)
+
 	pkg.CreateServer(routes, port)
 }
+
+
+deploy 1 : OK
+
+deploy 2 : failed
+
+deploy 3: ok
